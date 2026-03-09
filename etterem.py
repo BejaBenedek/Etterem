8ba379c5580365a5b716b8ac96771243bec491aa
Beja <beja.benedek2009@tanulo.boronkay.hu>
2026-03-09 13:40:05 +0100

    raktarfajl

--- etterem.py
+++ etterem.py
@@ -8,4 +8,9 @@ for sor in menufajl:
     bemenet = sor.strip().split(";")
     menu.append(bemenet)
 
+raktarfajl = open("raktar.csv","r",encoding="utf-8")
+for sor in raktarfajl:
+    bemenet = sor.strip().split(";")
+    raktar.append(bemenet)
+
 print(menu)
