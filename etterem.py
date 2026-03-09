11a1b1a6c359be0e170816724913a3d1f7d42590
PLaci1 <panczel.laszlo2009@tanulo.boronkay.hu>
2026-03-09 13:34:13 +0100

    menü tömb

--- etterem.py
+++ etterem.py
@@ -3,5 +3,9 @@ menu = []
 raktar = []
 vasarlok = []
 i = 0
-with open ("menu.csv" "r" encoding="utf-8") as fajl:
-    
+menufajl = open("menu.csv","r",encoding="utf-8")
+for sor in menufajl:
+    bemenet = sor.strip().split(";")
+    menu.append(bemenet)
+
+print(menu)
