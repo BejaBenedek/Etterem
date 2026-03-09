2c7bab35ff4e9f32278ea6dffca221fdf23cec56
PLaci1 <panczel.laszlo2009@tanulo.boronkay.hu>
2026-03-09 13:57:23 +0100

    coomit

--- etterem.py
+++ etterem.py
@@ -18,4 +18,7 @@ for sor in raktarfajl:
     bemenet = sor.strip().split(";")
     raktar.append(bemenet)
 
-
+vasarlofajl = open("raktar.csv","r",encoding="utf-8")
+for sor in vasarlofajl:
+    bemenet = sor.strip().split(";")
+    raktar.append(bemenet)
