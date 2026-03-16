ac5ac4685aca0589528720c41c9f1f7ecb8de11e
PLaci1 <panczel.laszlo2009@tanulo.boronkay.hu>
2026-03-16 14:05:40 +0100

    elkezdtem a keretrendszert

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import recept
 import raktar
 import vasarlok
 import lezart_rendeles
+import os
+
+osszesasztal = []
+
+asztalok_szama = 12
+tobbasztal = str(input("lesz több asztal? "))
+
+if tobbasztal == "igen":
+    plusz = int(input("hány új asztal lesz? "))
+    asztalok_szama += plusz
+else:
+    asztalok_szama += 0
+
+print(asztalok_szama, "asztal van")
+
+
+while len(osszesasztal) < asztalok_szama:
+    osszesasztal.append()
