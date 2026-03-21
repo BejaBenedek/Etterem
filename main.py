76a4180d2c07f098e604935dcca016311f6f8a77
ECLVision <visionapexalt@gmail.com>
2026-03-21 19:41:44 +0100

    asztalok listába írása

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ import lezart_rendeles
 import os
 
 class asztal:
-    def __init__(self, rendelesek_szama, teljes_osszeg):
+    def __init__(self,neve, rendelesek_szama, teljes_osszeg):
+        self.neve = neve
         self.rendelesek_szama = rendelesek_szama
         self.teljes_osszeg = teljes_osszeg
     
     def __str__(self):
-        return f"{self.rendelesek_szama}, {self.teljes_osszeg}"
+        return f"{self.neve}, {self.rendelesek_szama}, {self.teljes_osszeg}"
 
 osszesasztal = []
 
 asztalok_szama = 12
-tobbasztal = str(input("lesz több asztal? "))
+tobbasztal = input("lesz több asztal? ")
 
 if tobbasztal == "igen":
     plusz = int(input("hány új asztal lesz? "))
     asztalok_szama += plusz
-else:
-    asztalok_szama += 0
 
 print(asztalok_szama, "asztal van")
 
+i = 0
+while i < asztalok_szama:
+    nev = "asztal " + str(i+1)
+    osszesasztal.append(asztal(nev, 0, 0))
+    i += 1
 
-while len(osszesasztal) < asztalok_szama:
-   osszesasztal.append(asztal)
+os.system("cls")
 
