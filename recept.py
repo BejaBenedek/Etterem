aee6df83577c7d467d31aecd706f171dc26c1b80
ECLVision <visionapexalt@gmail.com>
2026-03-21 18:52:20 +0100

    kiiras fajlba recepteknel

--- recept.py
+++ recept.py
@@ -1,24 +1,36 @@
-recepteklista = []
-
-receptfajl = open("recept.csv","r", encoding="utf-8")
-for sor in receptfajl:
-    bemenet = sor.strip().split(";")
-    recepteklista.append(bemenet)
-
-def torles(receptek):
+def torles():
     ujreceptek = []
     
+    recepteklista = []
+
+    receptfajl = open("recept.csv","r", encoding="utf-8")
+    for sor in receptfajl:
+        bemenet = sor.strip().split(";")
+        recepteklista.append(bemenet)
+    receptfajl.close()
     ker = str(input("melyik receptet szeretnéd törölni? "))
     i = 0
 
-    while i < len(receptek):
-        if receptek[i][0] != ker:
-            ujreceptek.append(receptek[i])
+    while i < len(recepteklista):
+        if recepteklista[i][0] != ker:
+            ujreceptek.append(recepteklista[i])
         i += 1
     
-    return ujreceptek
+    receptkiiras = open("recept.csv", "w", encoding="utf-8")
+    i = 0
+    while i < len(ujreceptek):
+        receptkiiras.write(f"{ujreceptek[i][0]};{ujreceptek[i][1]};{ujreceptek[i][2]}\n")
+        i += 1
+    receptkiiras.close()
 
-def hozzaadas(receptek):
+def hozzaadas():
+    recepteklista = []
+
+    receptfajl = open("recept.csv","r", encoding="utf-8")
+    for sor in receptfajl:
+        bemenet = sor.strip().split(";")
+        recepteklista.append(bemenet)
+    receptfajl.close()
     hozzavalo = []
 
     nev = str(input("mi a recept neve? "))
@@ -33,10 +45,15 @@ def hozzaadas(receptek):
             hozzavalo.append(nev)
             hozzavalo.append(a)
             hozzavalo.append(b)
-            receptek.append(hozzavalo)    
+            recepteklista.append(hozzavalo)    
             hozzavalo = []
             i += 1
         i = 0  
         z += 1
 
-    return receptek
+    receptkiiras = open("recept.csv", "w", encoding="utf-8")
+    i = 0
+    while i < len(recepteklista):
+        receptkiiras.write(f"{recepteklista[i][0]};{recepteklista[i][1]};{recepteklista[i][2]}\n")
+        i += 1
+    receptkiiras.close()
