dae47e4ad949f928f7c8fdd538acd62d612f5ae6
Beja <beja.benedek2009@tanulo.boronkay.hu>
2026-03-23 14:30:46 +0100

    Feri pincér

--- vasarlok.py
+++ vasarlok.py
@@ -1,9 +1,20 @@
+import random
 vasarlok = []
 pincerek = []
 
 vasarlofajl = open("vasarlok.csv","r",encoding="utf-8")
 for sor in vasarlofajl:
     bemenet = sor.strip().split(";")
-    vasarlok.append(bemenet)
     pincerek.append(bemenet[1])
 
+jon = True
+while jon:
+    ujvendeg = input("Jött új vendég?")
+    if ujvendeg =="igen":
+        nev = input("Milyen névre foglalt")
+        vasarlok.append(nev)
+        vasarlok.append(random.choice(pincerek))
+    if ujvendeg == "nem":
+        jon = False
+
+print(vasarlok)
